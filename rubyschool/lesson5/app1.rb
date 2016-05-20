0f12c13fbf5c51ae2fbc2820ca798097de15579a
PinkDeer <bunman@mail.ru>
2016-05-20 08:06:47 +0300

    Update rubyschool

--- rubyschool/lesson5/app1.rb
+++ rubyschool/lesson5/app1.rb
@@ -1,1 +1,7 @@
 4.upto(20) {|x| puts x}
+
+-5.upto(5) {|x| puts x}
+
+50.downto(-50) {|x| puts x}
+
+10.downto(3) {|x| puts x} #3.downto(10) {|x| puts -x}
--- rubyschool/lesson5/app2.rb
+++ rubyschool/lesson5/app2.rb
@@ -1,1 +1,7 @@
--5.upto(5) {|x| puts x}
+print "Введите ваше имя: "
+name = gets.strip.capitalize
+# name.capitalize!
+puts "Привет, \t#{name}"
+puts "В вашем имени \t#{name.length} букв"
+
+# puts "Привет, \t#{name} \nВ вашем имени \t#{name.length} букв"
