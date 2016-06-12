7f4e8cf9acfd6d4d0a48ad7acfa4bf4f6c54a3a7
PinkDeer <bunman@mail.ru>
2016-06-12 14:04:10 +0300

    Update RubySchool

--- rubyschool/lesson17/app16.rb
+++ rubyschool/lesson17/app16.rb
@@ -1,15 +1,20 @@
 input = File.open "task.txt", "r"
 
 total = 0
+x = 0
 
 while (line = input.gets)
 
   arr = line.split ","
   value = arr[1].to_i
+  x = x + 1
+  if x == 6
+    puts "Total for 6 mounths: #{total}"
+  end
   total = total + value
 
 end
 
 input.close
 
-puts "Total: #{total}"
+puts "Total for the year: #{total}"
