9c364a6c0f8d93cffa1f3d0d010571588e3e339a
PinkDeer <bunman@mail.ru>
2018-02-26 21:48:31 +0300

    Update Learntoprogram

--- learntoprogram/chapter06/hw01.rb
+++ learntoprogram/chapter06/hw01.rb
@@ -1,47 +1,15 @@
-
-beer = 100
-
-until (beer == 1)
-	beer = beer - 1
-		puts  beer.to_s + ' бутылок пива на стене'
-		puts  beer.to_s + ' бутылок пива!'
-		puts 'Возьму одну, пусти по кругу'
-		puts beer.to_s + ' ,бутылок пива на стене!'
-		puts ''
-
+beer = 99
+while  beer != 1
+  puts "#{beer} bottles of beer on the wall"
+  puts "#{beer} bottles of beer!"
+  puts "Take beer down, pass it around"
+  beer = beer - 1
+  puts "#{beer} bottles of beer on the wall!"
+  puts ""
 end
 
-
-# черновик
-
-=begin
-
-beer = 100
-until (beer == 1)
-	beer = beer - 1
-	if beer >= 95
-		puts  beer.to_s + ' бутылок пива на стене'
-		puts  beer.to_s + ' бутылок пива!'
-		puts 'Возьму одну, пусти по кругу'
-		puts beer.to_s + ' ,бутылок пива на стене!'
-		puts ''
-	else
-		if
-			beer >= 92
-			puts  beer.to_s + ' бутылки пива на стене'
-			puts  beer.to_s + ' бутылки пива!'
-			puts 'Возьму одну, пусти по кругу'
-			puts beer.to_s + ' ,бутылки пива на стене!'
-			puts ''
-
-		else
-			puts  beer.to_s + ' бутылка пива на стене'
-			puts  beer.to_s + ' бутылка пива!'
-			puts 'Возьму одну, пусти по кругу'
-			puts beer.to_s + ' ,бутылка пива на стене!'
-			puts ''
-		end
-	end
-end
-
-=end
+puts "#{beer} bottle of beer on the wall"
+puts "#{beer} bottle of beer!"
+puts "Take beer down, pass it around"
+beer = beer - 1
+puts "#{beer} bottles of beer on the wall!"
