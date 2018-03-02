c26fb3be4b422a60b2fbd38129e39716fb87f8e2
PinkDeer <bunman@mail.ru>
2018-03-02 15:28:58 +0300

    Update Learntoprogram

--- learntoprogram/chapter05/hw03.rb
+++ learntoprogram/chapter05/hw03.rb
@@ -4,4 +4,4 @@ lineWidth = 50
 puts 'Содержание'.center lineWidth
 puts 'Глава 1:  Числа'.ljust(lineWidth/2) + 'page 1'.rjust(lineWidth/2)
 puts 'Глава 2:  Буквы'.ljust(lineWidth/2) + 'page 72'.rjust(lineWidth/2)
-puts 'Глава 3:  Переменные'.ljust(lineWidth/2) + 'page 11'.rjust(lineWidth/2)
+puts 'Глава 3:  Переменные'.ljust(lineWidth/2) + 'page 118'.rjust(lineWidth/2)
